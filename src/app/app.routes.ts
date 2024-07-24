e638c1137d06b8748bb3ac696d361f4b3964fe47
Jorge Morales <21000388@alumnos.utleon.edu.mx>
2024-07-23 22:52:34 -0600

    Se sube modulo de productos, con APIs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProviderListComponent } from './components/proveedor/provider-list/provider-list.component';
 import { ProviderDetailsComponent } from './components/proveedor/provider-details/provider-details.component';
+import { ProductoListComponent } from './components/producto/producto-list/producto-list.component';
 
 export const routes: Routes = [
     { path: 'proveedores', component: ProviderListComponent },
     { path: 'proveedores/details/:id', component: ProviderDetailsComponent },
+    { path: 'productos', component: ProductoListComponent },
 ];
 
