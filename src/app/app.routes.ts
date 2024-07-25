0445967c4501fdfd91aaf4fb4cd52222d9a21c23
DiegoJC21 <dajc.2112@gmail.com>
2024-07-24 20:02:51 -0600

    Login html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProviderListComponent } from './components/proveedor/provider-list/provider-list.component';
 import { ProviderDetailsComponent } from './components/proveedor/provider-details/provider-details.component';
+import { LoginFormComponent } from './components/login/login-form/login-form.component';
+
 
 export const routes: Routes = [
     { path: 'proveedores', component: ProviderListComponent },
     { path: 'proveedores/details/:id', component: ProviderDetailsComponent },
+    { path: 'login', component: LoginFormComponent },
 ];
 
