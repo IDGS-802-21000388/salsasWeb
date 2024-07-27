2ad67a2ff6cb9199203f6fc8a15b96bd3fff726b
Jorge Morales <21000388@alumnos.utleon.edu.mx>
2024-07-27 14:40:36 -0600

    Se sube modulo de comparacion de compras y ventas terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProveedorModule } from './components/proveedor/proveedor.module';
 import { SalsasReniModule } from "./salsas-reni/salsas-reni.module";
 import { ProductoModule } from './components/producto/producto.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ComparacionModule } from './components/comparacion/comparacion.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HttpClientModule, ProveedorModule,ProductoModule, SalsasReniModule],
+  imports: [RouterOutlet, RouterLink, HttpClientModule, ProveedorModule,ProductoModule, SalsasReniModule, ComparacionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProviderListComponent } from './components/proveedor/provider-list/provider-list.component';
 import { ProviderDetailsComponent } from './components/proveedor/provider-details/provider-details.component';
 import { ProductoListComponent } from './components/producto/producto-list/producto-list.component';
+import { ComparacionComponent } from './components/comparacion/comparacion/comparacion.component';
 
 export const routes: Routes = [
     { path: 'proveedores', component: ProviderListComponent },
     { path: 'proveedores/details/:id', component: ProviderDetailsComponent },
     { path: 'productos', component: ProductoListComponent },
+    { path: 'comparacion', component: ComparacionComponent },
 ];
 
