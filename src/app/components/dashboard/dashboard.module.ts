12ef7eb9facaaad589e8a89941ca17c33793b9c5
EdwinY-a <21000445@alumnos.utleon.edu.mx>
2024-08-02 14:18:42 -0600

    Modulo dashboard

---
+++ src/app/components/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardListComponent } from './dashboard-list/dashboard-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
