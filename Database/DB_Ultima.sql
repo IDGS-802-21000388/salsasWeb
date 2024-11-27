0fed4423c60f8d1feabf98bc5ded48c972b8db3e
DiegoJC21 <dajc.2112@gmail.com>
2024-11-26 22:50:12 -0600

    Cambios en tabla Empresa y nueva tabla EmpresaUsuario

--- Database/DB_Ultima.sql
+++ Database/DB_Ultima.sql
@@ -38,6 +38,21 @@ CREATE TABLE Usuario (
 );
 GO
 
+CREATE TABLE Empresa (
+    idEmpresa INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(10),
+    idDireccion INT NOT NULL,
+    FOREIGN KEY (idDireccion) REFERENCES Direccion(idDireccion)
+);
+CREATE TABLE EmpresaUsuario (
+    idEmpresaUsuario INT IDENTITY(1,1) PRIMARY KEY,
+    idEmpresa INT NOT NULL,
+    idUsuario INT NOT NULL,
+    FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+
 CREATE TABLE AgentesVenta (
     idAgentesVenta INT IDENTITY(1,1) PRIMARY KEY,
     idAgente INT NOT NULL,
@@ -46,15 +61,6 @@ CREATE TABLE AgentesVenta (
     FOREIGN KEY (idCliente) REFERENCES Usuario(idUsuario)
 );
 
-CREATE TABLE Empresa(
-	idEmpresa INT IDENTITY(1,1) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-	telefono VARCHAR(10),
-	idUsuario INT NOT NULL,
-	idDireccion INT NOT NULL,      
-	FOREIGN KEY (idDireccion) REFERENCES Direccion(idDireccion),
-	FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
-);
 
 CREATE TABLE LogsUser (
     id INT IDENTITY(1,1) PRIMARY KEY,
